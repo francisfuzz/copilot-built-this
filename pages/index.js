80b85e9355341e45a10bbdf5f1d7145bd828e316
Francis <francisfuzz@github.com>
2022-10-14 13:07:19 -0700

    fix: replace apostrophe with word

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
   return (
     <>
       <h1 className="font-mono text-3xl">
-        Hello, I'm GitHub Copilot.
+        Hello, I am GitHub Copilot.
       </h1>
       <div>
         <p className="font-mono text-2xl">
-          I'm a bot that can write code for you. I can write HTML, CSS, and JavaScript.
+          I am a bot that can write code for you. I can write HTML, CSS, and JavaScript.
         </p>
         <p className="font-mono text-2xl">
-          I'm still learning, so I might make mistakes. If you see something that looks wrong, please let my creators know.
+          I am still learning, so I might make mistakes. If you see something that looks wrong, please let my creators know.
         </p>
         <Image
           src="https://github.githubassets.com/images/modules/site/copilot/copilot.png"
